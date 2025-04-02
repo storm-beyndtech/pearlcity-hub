84eb8206bfe4680b1bfeb12a7c85b0c130087059
storm <khameleonstorm@gmail.com>
2025-04-02 11:19:05 +0100

    Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Pricing from "./pages/Pricing";
-import FAQ from "./pages/FAQ";
-import About from "./pages/About";
+// import Services from "./pages/Services";
+// import Pricing from "./pages/Pricing";
+// import FAQ from "./pages/FAQ";
+// import About from "./pages/About";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
-			<Route path="/services" element={<Services />} />
-			<Route path="/about" element={<About />} />
-			<Route path="/pricing" element={<Pricing />} />
-      <Route path="/faq" element={<FAQ />} />
-      
-      <Route path="*" element={<Navigate to="/" />} />
+			<Route path="/services" element={<Home />} />
+			<Route path="/about" element={<Home />} />
+			<Route path="/pricing" element={<Home />} />
+			<Route path="/faq" element={<Home />} />
+
+			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	);
 }
